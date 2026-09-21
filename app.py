afd1af3d62dde84b8e7446af62b4444f59c70846
valentxt <valentinofebriyan07@gmail.com>
2026-09-21 09:41:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -452,8 +452,7 @@ def analyze():
             "waktu_uji": waktu_uji
         })
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
-
+    if __name__ == '__main__':
+    app.run()
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
